f5f195023ba424ca4ddf543bd5c5b30d3cd8a0bc
Tran Cong Tru <trutc@innotech-vn.com>
2022-06-27 13:41:38 +0700

    1

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world tru tran');
+  res.send('Hello world 1111');
 });
 
 app.listen(PORT);
