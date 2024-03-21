a3a8a9124431b89c8ab9c6634ad72b9d83ea536e
Tru Tran <congtruqn@gmail.com>
2024-03-21 16:56:02 +0700

    update

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 3000;
 // App
 const app = express();
 app.get('/', async function (req, res) {
-  const test = await axios.get('https://tns.vn');
-  console.log(test)
+  //const test = await axios.get('https://tns.vn');
+  //console.log(test)
   res.send('test');
 });
 
