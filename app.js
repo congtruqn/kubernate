f5e105bcc1516bc9da854d8c642728159ee4e99e
Tru Tran <congtruqn@gmail.com>
2024-03-21 17:41:02 +0700

    update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world 1');
+  res.send('Hello world 11');
 });
 
 app.listen(PORT);
